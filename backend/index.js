18a04c727f36a078764551f12a1dae83505eac13
Kurisu081299 <robertchristian.rosales@gmail.com>
2023-01-10 01:26:36 +0800

    intial commit

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+import express from "express"
+import mysql from "mysql"
+
+const app = express()
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"135&robert081299",
+    database:"test"
+})
+
+
+
+app.use(express.json())
+
+app.get("/", (req, res)=>{
+    res.json("Welcome to Books Database")
+})
+
+app.get("/books", (req, res)=>{
+    const q = "SELECT * FROM books"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.post("/books", (req, res)=>{
+    const q = "INSERT INTO books (`title`,`desc`,`cover`) VALUES (?)"
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.cover,
+    ];
+
+    db.query(q,[values], (err,data)=>{
+        if(err) return res.json(err)
+        return res.json("Created a book entry!")
+    })
+})
+
+app.listen(8080, ()=>{
+    console.log("Connected to Database")
+})
